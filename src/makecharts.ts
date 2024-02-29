566ed86d926cf182a7762366e738056a487d7e89
ameliamnesia <133761448+ameliamnesia@users.noreply.github.com>
2024-02-29 13:52:32 -0500

    stats paging layout tweaks

--- src/makecharts.ts
+++ src/makecharts.ts
@@ -1,10 +1,7 @@
 export const baseUrl = 'https://skeetstats.xyz'
-// Use window.location to get the current URL
 const urlString: string = window.location.href;
 const url = new URL(urlString);
-// Extract the "handle" portion from the pathname
 const handle = url.pathname.split("/").pop() || '';
-// Remove '@', apostrophes, and quotation marks from the handle
 const cleanedHandle = handle.replace(/[@'"]/g, '');
 export const user: string = cleanedHandle || 'skeetstats.xyz';
 import { makeCharts } from "./charts.js";
